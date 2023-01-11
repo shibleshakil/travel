89f2627799419d80a28beba525384f28ec140ff2
shibleshakil <shakil.shible@gmail.com>
2023-01-11 17:27:04 +0600

    hotel module start

---
+++ app/Http/Controllers/Admin/Module/HotelController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Module;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class HotelController extends Controller
+{
+    //
+}
---
+++ app/Models/Hotel.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Hotel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'hotels';
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'location',
+        'location',
+    ];
+}
